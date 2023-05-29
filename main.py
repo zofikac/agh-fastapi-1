b93bc27205907ea8e5040c19a722668d06174fa4
Michał Gacek <gacek.michael@outlook.com>
2023-05-29 08:52:59 +0200

    feat: add support for CORS

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import students
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(students.router, prefix="/students")
